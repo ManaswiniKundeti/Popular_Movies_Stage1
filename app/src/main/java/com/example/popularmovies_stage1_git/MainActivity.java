bdcc3f9371cbad6c4b3cc19c6d04c13d02d90506
Manaswini Kundeti <manaswini.kundeti@gmail.com>
2019-05-18 17:40:55 -0700

    popup_menu for display of top_rated and popular movies

--- app/src/main/java/com/example/popularmovies_stage1_git/MainActivity.java
+++ app/src/main/java/com/example/popularmovies_stage1_git/MainActivity.java
@@ -5,9 +5,14 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -27,6 +32,10 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    final static String POPULAR = "popular";
+
+    final static String TOP_RATED = "top_rated";
+
     public List<Movie> movieList = new ArrayList<Movie>();
     GridAdapter movieAdapter;
     private ProgressBar movieProgressBar;
@@ -36,18 +45,48 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        GridView movieGridDisplay = findViewById(R.id.movie_grid_data);
+        GridView mGridView = findViewById(R.id.movie_grid_data);
         movieAdapter = new GridAdapter(this, movieList);
-        movieGridDisplay.setAdapter(movieAdapter);
+        mGridView.setAdapter(movieAdapter);
+
+        mGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Movie selectedMovie = movieAdapter.getItem(position);
+                
+            }
+        });
 
         movieProgressBar = findViewById(R.id.movie_progress_bar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.popup_menu, menu);
+        return true;
+    }
+
+    public void onItemClick(MenuItem item){
+
+        if(item.getItemId() == R.id.item1) {
+            Toast toast = Toast.makeText(getApplicationContext(), "Item 1 clicked", Toast.LENGTH_SHORT);
+            toast.show();
+
+            new NetworkUtils().execute(POPULAR);
+
+        } else if (item.getItemId() == R.id.item2) {
+            Toast toast = Toast.makeText(getApplicationContext(), "Item 2 clicked", Toast.LENGTH_SHORT);
+            toast.show();
+            new NetworkUtils().execute(TOP_RATED);
+        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         if (movieList.isEmpty()) {
-            new NetworkUtils().execute();
+            new NetworkUtils().execute(POPULAR);
         }
     }
 
@@ -55,14 +94,17 @@ public class MainActivity extends AppCompatActivity {
         movieProgressBar.setVisibility(isLoading ? View.VISIBLE : View.GONE);
     }
 
-    public class NetworkUtils extends AsyncTask<Void, Void, String>{
+    public class NetworkUtils extends AsyncTask<String, Void, String>{
 
-        final static String BASE_URL = "http://api.themoviedb.org/3/movie/popular";
+        final static String BASE_URL = "http://api.themoviedb.org/3/movie";
 
         final static String API_KEY = "fb818f2b738d211782879135fc73eed7";
 
-        public URL buildUrl() {
-            Uri builtUri = Uri.parse(BASE_URL).buildUpon().appendQueryParameter("api_key",API_KEY)
+
+        public URL buildUrl(String string) {
+            Uri builtUri = Uri.parse(BASE_URL).buildUpon()
+                    .appendPath(string)
+                    .appendQueryParameter("api_key",API_KEY)
                     .build();
             URL url = null;
             try {
@@ -80,8 +122,9 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
-        protected String doInBackground(Void... voids) {
-            URL url = buildUrl();
+        protected String doInBackground(String... strings) {
+            String myParam = strings[0];
+            URL url = buildUrl(myParam);
             StringBuilder sb = new StringBuilder();
             try {
                 HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
@@ -107,6 +150,8 @@ public class MainActivity extends AppCompatActivity {
             Log.d(TAG, data);
 
             try{
+                movieList.clear();
+
                 JSONArray results = new JSONObject(data).getJSONArray("results");
 
                 for(int iter = 0; iter < results.length(); iter++){
